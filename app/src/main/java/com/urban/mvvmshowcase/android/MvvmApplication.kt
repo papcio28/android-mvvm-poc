0f075649e1ad92c2fe2b810a6c35cc41378c1dbb
Paweł Urban <papcio28@gmail.com>
2017-07-28 12:48:12 +0200

    Added interface type to personRepository in MvvmApplication.

--- app/src/main/java/com/urban/mvvmshowcase/android/MvvmApplication.kt
+++ app/src/main/java/com/urban/mvvmshowcase/android/MvvmApplication.kt
@@ -3,9 +3,10 @@ package com.urban.mvvmshowcase.android
 import android.app.Application
 import android.content.Context
 import com.urban.mvvmshowcase.model.repository.FakePersonRepository
+import com.urban.mvvmshowcase.model.repository.PersonRepository
 
 class MvvmApplication : Application() {
-    val personRepository = FakePersonRepository
+    val personRepository: PersonRepository = FakePersonRepository
 
     companion object {
         @JvmStatic
