e0cebfda90b0c3e9c2f91d2e55acadbd066be60b
Paweł Urban <papcio28@gmail.com>
2017-07-28 14:16:42 +0200

    StandardOneLineViewHolder ported to Kotlin.

---
+++ app/src/main/java/com/urban/mvvmshowcase/view/adapter/StandardOneLineViewHolder.kt
@@ -0,0 +1,27 @@
+package com.urban.mvvmshowcase.view.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.urban.mvvmshowcase.model.entity.Person
+
+class StandardOneLineViewHolder(container: ViewGroup, private val clickListener: ClickListener) :
+        RecyclerView.ViewHolder(LayoutInflater.from(container.context)
+                .inflate(android.R.layout.simple_list_item_1, container, false)),
+        View.OnClickListener {
+    private lateinit var person: Person
+    private val textLine = itemView.findViewById(android.R.id.text1) as TextView
+
+    init {
+        itemView.setOnClickListener(this)
+    }
+
+    fun bind(person: Person) {
+        textLine.text = person.toString()
+        this.person = person
+    }
+
+    override fun onClick(v: View?) = clickListener.onPersonClick(person)
+}
